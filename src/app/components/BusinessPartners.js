b4838e5c142c112b5849c41fafb6bb532f5b78cd
/Masoud <masoud_tesna@live.com>
2023-10-27 22:20:25 +0330

    create BusinessPartners section (#4)...

---
+++ src/app/components/BusinessPartners.js
@@ -0,0 +1,23 @@
+export default () => {
+  return (
+    <>
+      <div className="text-center">
+        <div className="text-black text-captionXxl">
+          همکاران تجاری هیراد
+        </div>
+        
+        <div className="text-gray-40 text-captionXl">
+          Hirad Partners
+        </div>
+      </div>
+      
+      <div className="mt-[30px] grid grid-cols-5 gap-[16px]">
+        {(new Array(10).fill(2))?.map((_, index) => (
+          <div key={index} className="bg-gray-20 h-[143px]">
+          
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
