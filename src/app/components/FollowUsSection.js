adb58624fdfdde8c59f03265f86745be1380b714
/Masoud <masoud_tesna@live.com>
2023-10-27 21:50:20 +0330

    create FollowUs section (#3)...

---
+++ src/app/components/FollowUsSection.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import {Button} from 'antd';
+
+export default () => {
+  return (
+    <div className="bg-primary shadow-4 flex flex-col justify-center items-center py-[30px]">
+      <div>
+        <Image
+          src={'/aargon»ogo.svg'}
+          alt={'alt'}
+          layout="FILL"
+          width={312}
+          height={280}
+          className="max-h-[280px] mt-[-50px]"
+        />
+      </div>
+      
+      <div>
+        <Button className="!bg-transparent !text-white !text-captionXl !tracking-[2.7px] !border-white !h-[47px] !px-[3rem]">
+          Follow Us
+        </Button>
+      </div>
+    </div>
+  );
+};
