1e86cd0162801b031b4dbb157400b06d727eecc8
/Masoud <masoud_tesna@live.com>
2023-12-20 20:19:21 +0330

    fix use generateStaticParams() bug for build...

--- next.config.js
+++ next.config.js
@@ -2,17 +2,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/dashboard',
-        destination: '/dashboard/users',
-        permanent: true
-      }
-    ];
-  },
-  
-  output: 'standalone'
+    async redirects() {
+        return [
+            {
+                source: '/dashboard',
+                destination: '/dashboard/users',
+                permanent: true
+            }
+        ];
+    },
+    output: 'standalone'
 };
 
 module.exports = nextConfig;
--- src/app/(basic)/projects/[projectName]/components/Description.js
+++ src/app/(basic)/projects/[projectName]/components/Description.js
@@ -1,38 +1,40 @@
+'use client';
+
 import {Button} from 'antd';
 
 const Description = ({details}) => {
-  return (
-    <>
-      <div
-        className="max-md:text-center text-[5.94306rem] bg-clip-text text-transparent uppercase font-bold opacity-50"
-        style={{
-          backgroundPosition: '-70% 70%',
-          backgroundRepeat: 'repeat, no-repeat',
-          backgroundImage: `url('${details?.projectImage}'), none`
-        }}
-      >
-        {details?.projectName}
-      </div>
-      
-      <div className="max-md:text-center text-gray-40 text-[1.51369rem] tracking-[0.115rem] capitalize">
-        {details.title.top}
-      </div>
-      
-      <div className="max-md:text-center text-black text-[1.329rem] tracking-[0.118rem] capitalize mb-[20px]">
-        {details?.title?.bottom}
-      </div>
-      
-      <div className="text-captionXl text-gray-40 text-justify line-clamp-[18]">
-        <p>
-          {details?.description}
-        </p>
-      </div>
-      
-      <Button type="primary" className="!bg-primary !shadow-3 mt-[20px]" block>
-        رزرو بازدید پروژه
-      </Button>
-    </>
-  );
+    return (
+        <>
+            <div
+                className="max-md:text-center text-[5.94306rem] bg-clip-text text-transparent uppercase font-bold opacity-50"
+                style={{
+                    backgroundPosition: '-70% 70%',
+                    backgroundRepeat: 'repeat, no-repeat',
+                    backgroundImage: `url('${details?.projectImage}'), none`
+                }}
+            >
+                {details?.projectName}
+            </div>
+
+            <div className="max-md:text-center text-gray-40 text-[1.51369rem] tracking-[0.115rem] capitalize">
+                {details.title.top}
+            </div>
+
+            <div className="max-md:text-center text-black text-[1.329rem] tracking-[0.118rem] capitalize mb-[20px]">
+                {details?.title?.bottom}
+            </div>
+
+            <div className="text-captionXl text-gray-40 text-justify line-clamp-[18]">
+                <p>
+                    {details?.description}
+                </p>
+            </div>
+
+            <Button type="primary" className="!bg-primary !shadow-3 mt-[20px]" block>
+                رزرو بازدید پروژه
+            </Button>
+        </>
+    );
 };
 
 export default Description;
