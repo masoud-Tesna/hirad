05259ddd4ad06a71cf6902dfbb6540140b30dcfc
masoud-tesna <masoud_tesna@live.com>
2024-02-14 11:54:04 +0330

    fix bug

--- src/templates/components/Content.js
+++ src/templates/components/Content.js
@@ -1,12 +1,22 @@
 'use client';
 
-import Banner from './Banner';
+// import Banner from './Banner';
 import BannerCTA from './BannerCTA';
 import classNames from 'classnames';
-import {Layout} from 'antd';
-import {useSelectedLayoutSegment} from 'next/navigation';
+import { Layout } from 'antd';
+import { useSelectedLayoutSegment } from 'next/navigation';
+import dynamic from 'next/dynamic';
+import Skeleton from 'react-loading-skeleton';
 
-const Content = ({children}) => {
+const Banner = dynamic(
+  () => import('./Banner'),
+  {
+    ssr: false,
+    loading: () => <Skeleton height={ 297 } width={ '100%' } />
+  }
+);
+
+const Content = ({ children }) => {
   const segment = useSelectedLayoutSegment();
   
   return (
@@ -25,12 +35,17 @@ const Content = ({children}) => {
         <Banner asMobile />
       </div>
       
-      <div className={classNames('w-full mx-auto relative md:hidden p-[16px] pb-0', {'!hidden': segment === 'projects'})}>
+      <div
+        className={ classNames(
+          'w-full mx-auto relative md:hidden p-[16px] pb-0',
+          { '!hidden': segment === 'projects' }
+        ) }
+      >
         <BannerCTA />
       </div>
       
       <div className="--pageSection md:mt-[40px] text-black w-full md:w-10/12 mx-auto">
-        {children}
+        { children }
       </div>
     </Layout.Content>
   );
